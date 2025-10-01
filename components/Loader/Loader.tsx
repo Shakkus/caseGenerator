c00cbd9e105b8a814e1c96c2e128505b48e1a980
Shakkus <sebadevalbornoz@gmail.com>
2025-09-30 22:57:28 -0300

    blablabla

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -5,6 +5,7 @@ const Loader = () => {
     <div className="flex flex-col justify-around items-center">
         <h2 className="text-4xl m-5 text-white">Generando caso . . .</h2>
         <Skeleton className="h-50 w-50 rounded-full bg-red-500 m-5"/>
+        <p className="text-gray-100">Esto puede llevar un rato . . .</p>
     </div>
   )
 }
